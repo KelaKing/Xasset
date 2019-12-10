106caaeddeb90adcf2d35905518c047bba9e61ec
KelaKing <jxwywxy@icloud.com>
2019-12-10 16:06:38 +0800

    Update README.md

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
     platforms: [
         .macOS(.v10_13),
     ],
+    products: [
+        .executable(name: "xasset", targets: ["Xasset"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/kareman/SwiftShell", from: "5.0.1"),
         .package(url: "https://github.com/kylef/Commander.git", from: "0.9.1"),
-    ],
-    products: [
-        .executable(name: "xasset", targets: ["Xasset"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Xasset
 
-A description of this package.
+Export images from xcassets and replace images to xcassets.
 
 ## Installing
 ### [Mint](https://github.com/yonaskolb/mint)
 ```
 $ mint install KelaKing/Xasset
+```
+## Usage
+```sh
+# Export images
+xasset export ~/Foo.xcassets ~/FooImages
+# Replace images
+xasset replace ~/FooImages ~/Foo.xcassets
 ```
